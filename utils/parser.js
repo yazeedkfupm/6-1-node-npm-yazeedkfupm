9f8828ae20a7b0b74df2e390c5e39e87190c3c28
yazeedkfupm <s202164390@kfupm.edu.sa>
2025-11-10 09:34:19 +0300

    task 5 9:34

--- utils/parser.js
+++ utils/parser.js
@@ -1,10 +1,12 @@
 import _ from "lodash";
 
 export function parseNumbers(input) {
-  return {/*write code here*/}
+  const numbers = _.map(input, (str) => Number(str));
+  return _.compact(numbers);
 }
 
 export function isValidOperation(operation) {
-  return {/*write code here*/}
+  const validOps = ["add", "subtract", "multiply", "divide"];
+  return _.includes(validOps, operation);
 }
 
